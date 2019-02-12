9559e67088f8fc8cb08bac8d9b4243a9982fdfa9
minwoo cho <minwoocho@minwoos-Air.fios-router.home>
2019-02-12 15:25:36 -0800

    edits

--- js/app.js
+++ js/app.js
@@ -42,71 +42,295 @@ var map;
 var markers = [];
 function initMap() {
   var styles = [
-          {
-            featureType: 'water',
-            stylers: [
-              { color: '#19a0d8' }
-            ]
-          },{
-            featureType: 'administrative',
-            elementType: 'labels.text.stroke',
-            stylers: [
-              { color: '#ffffff' },
-              { weight: 6 }
-            ]
-          },{
-            featureType: 'administrative',
-            elementType: 'labels.text.fill',
-            stylers: [
-              { color: '#e85113' }
-            ]
-          },{
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [
-              { color: '#efe9e4' },
-              { lightness: -40 }
-            ]
-          },{
-            featureType: 'transit.station',
-            stylers: [
-              { weight: 9 },
-              { hue: '#e85113' }
-            ]
-          },{
-            featureType: 'road.highway',
-            elementType: 'labels.icon',
-            stylers: [
-              { visibility: 'off' }
-            ]
-          },{
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [
-              { lightness: 100 }
-            ]
-          },{
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [
-              { lightness: -100 }
-            ]
-          },{
-            featureType: 'poi',
-            elementType: 'geometry',
-            stylers: [
-              { visibility: 'on' },
-              { color: '#f0e4d3' }
-            ]
-          },{
-            featureType: 'road.highway',
-            elementType: 'geometry.fill',
-            stylers: [
-              { color: '#efe9e4' },
-              { lightness: -25 }
-            ]
-          }
-        ];
+    {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#165c64"
+            },
+            {
+                saturation: 34
+            },
+            {
+                lightness: -69
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    },
+    {
+        featureType: "landscape",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#b7caaa"
+            },
+            {
+                saturation: -14
+            },
+            {
+                lightness: -18
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    },
+    {
+        featureType: "landscape.man_made",
+        elementType: "all",
+        stylers: [
+            {
+                hue: "#cbdac1"
+            },
+            {
+                saturation: -6
+            },
+            {
+                lightness: -9
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    },
+    {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#8d9b83"
+            },
+            {
+                saturation: -89
+            },
+            {
+                lightness: -12
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    },
+    {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#d4dad0"
+            },
+            {
+                saturation: -88
+            },
+            {
+                lightness: 54
+            },
+            {
+                visibility: "simplified"
+            }
+        ]
+    },
+    {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#bdc5b6"
+            },
+            {
+                saturation: -89
+            },
+            {
+                lightness: -3
+            },
+            {
+                visibility: "simplified"
+            }
+        ]
+    },
+    {
+        featureType: "road.local",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#bdc5b6"
+            },
+            {
+                saturation: -89
+            },
+            {
+                lightness: -26
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    },
+    {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#c17118"
+            },
+            {
+                saturation: 61
+            },
+            {
+                lightness: -45
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    },
+    {
+        featureType: "poi.park",
+        elementType: "all",
+        stylers: [
+            {
+                hue: "#8ba975"
+            },
+            {
+                saturation: -46
+            },
+            {
+                lightness: -28
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    },
+    {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#a43218"
+            },
+            {
+                saturation: 74
+            },
+            {
+                lightness: -51
+            },
+            {
+                visibility: "simplified"
+            }
+        ]
+    },
+    {
+        featureType: "administrative.province",
+        elementType: "all",
+        stylers: [
+            {
+                hue: "#ffffff"
+            },
+            {
+                saturation: 0
+            },
+            {
+                lightness: 100
+            },
+            {
+                visibility: "simplified"
+            }
+        ]
+    },
+    {
+        featureType: "administrative.neighborhood",
+        elementType: "all",
+        stylers: [
+            {
+                hue: "#ffffff"
+            },
+            {
+                saturation: 0
+            },
+            {
+                lightness: 100
+            },
+            {
+                visibility: "off"
+            }
+        ]
+    },
+    {
+        featureType: "administrative.locality",
+        elementType: "labels",
+        stylers: [
+            {
+                hue: "#ffffff"
+            },
+            {
+                saturation: 0
+            },
+            {
+                lightness: 100
+            },
+            {
+                visibility: "off"
+            }
+        ]
+    },
+    {
+        featureType: "administrative.land_parcel",
+        elementType: "all",
+        stylers: [
+            {
+                hue: "#ffffff"
+            },
+            {
+                saturation: 0
+            },
+            {
+                lightness: 100
+            },
+            {
+                visibility: "off"
+            }
+        ]
+    },
+    {
+        featureType: "administrative",
+        elementType: "all",
+        stylers: [
+            {
+                hue: "#3a3935"
+            },
+            {
+                saturation: 5
+            },
+            {
+                lightness: -57
+            },
+            {
+                visibility: "off"
+            }
+        ]
+    },
+    {
+        featureType: "poi.medical",
+        elementType: "geometry",
+        stylers: [
+            {
+                hue: "#cba923"
+            },
+            {
+                saturation: 50
+            },
+            {
+                lightness: -46
+            },
+            {
+                visibility: "on"
+            }
+        ]
+    }
+];
     document.getElementById('show-listings').addEventListener('click', showListings);
     document.getElementById('hide-listings').addEventListener('click', function() {
       hideMarkers(markers);
@@ -130,7 +354,7 @@ function initMap() {
       var title = location.info;
       var text = location.name;
 
-      globalInfowindows = infoWindowCreate();
+      globalInfowindows = infoWindowCreate(); //created a infowindow inside for loops so it'll be accessible for models in the array any where..?
 
       var marker = setMarker(map, position, title, text, defaultIcon, i);
 
@@ -140,9 +364,7 @@ function initMap() {
       bounds.extend(markers[i].position);
 
       marker.addListener('click', function(id) {
-        //if (this.id == i) {
         location = locations[this.id];
-        //}
         vm.setLocation(location);
         populateInfoWindow(this, globalInfowindows);
 
